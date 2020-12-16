1a488ad49a6a203ee9a746cb0f7d7975aeedcb44
lucy0620 <lucy0620qqq123@163.com>
2020-12-16 14:26:01 +0800

    新建两个组件在view中

---
+++ lucyh/src/views/Ablout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        关于
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Ablout"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ lucyh/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        我的home
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
